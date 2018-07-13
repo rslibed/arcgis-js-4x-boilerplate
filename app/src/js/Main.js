4466f0b98faa3c6255a9f9808ee043f22569101c
Ryan Libed <ryan0000@esri.com>
2018-07-12 17:24:58 -0700

    Created sketch UI for SketchViewModel module

--- app/src/js/Main.js
+++ app/src/js/Main.js
@@ -1,2 +1,42 @@
-require(["dojo/domReady!"], function () { });
+require([
+    "esri/WebMap",
+    "esri/views/MapView",
+    "esri/widgets/Legend",
+    "esri/widgets/Expand",
+    "esri/widgets/Search",
+    "app/Sketch/SketchWidget",
+    "esri/layers/GraphicsLayer",
+    "dojo/domReady!"
+], function (WebMap, MapView, Legend, Expand, Search, SketchWidget, GraphicsLayer) {
+    var graphicsLayer = new GraphicsLayer();
+    var webmap = new WebMap({
+        portalItem: {
+            id: "1354d582ae87490dbb3fbaf4010e58f6"
+        },
+        layers: [graphicsLayer]
+    });
+    var view = new MapView({
+        map: webmap,
+        container: "viewDiv"
+    });
+    var legend = new Legend({
+        view: view
+    });
+    var legendNode = document.createElement("div");
+    var legendExpand = new Expand({
+        container: legendNode,
+        content: legend
+    });
+    var search = new Search({
+        view: view
+    });
+    var sketchContainer = document.createElement("div");
+    var sketch = new SketchWidget({
+        container: sketchContainer,
+        view: view,
+        layer: graphicsLayer
+    });
+    var group = [sketch, legendExpand];
+    view.ui.add(group, "top-right");
+});
 //# sourceMappingURL=Main.js.map
--- app/src/js/Main.ts
+++ app/src/js/Main.ts
@@ -1,1 +1,49 @@
-require(["dojo/domReady!"], function() {});
+require([
+  "esri/WebMap",
+  "esri/views/MapView",
+  "esri/widgets/Legend",
+  "esri/widgets/Expand",
+  "esri/widgets/Search",
+  "app/Sketch/SketchWidget",
+  "esri/layers/GraphicsLayer", // Be sure to point directly to JS file
+  "dojo/domReady!"
+], function(
+  WebMap,
+  MapView,
+  Legend,
+  Expand,
+  Search,
+  SketchWidget,
+  GraphicsLayer
+) {
+  const graphicsLayer = new GraphicsLayer();
+  const webmap = new WebMap({
+    portalItem: {
+      id: "1354d582ae87490dbb3fbaf4010e58f6"
+    },
+    layers: [graphicsLayer]
+  });
+  const view = new MapView({
+    map: webmap,
+    container: "viewDiv"
+  });
+  const legend = new Legend({
+    view
+  });
+  const legendNode = document.createElement("div");
+  const legendExpand = new Expand({
+    container: legendNode,
+    content: legend
+  });
+  const search = new Search({
+    view
+  });
+  const sketchContainer = document.createElement("div");
+  const sketch = new SketchWidget({
+    container: sketchContainer,
+    view,
+    layer: graphicsLayer
+  });
+  const group = [sketch, legendExpand];
+  view.ui.add(group, "top-right");
+});
