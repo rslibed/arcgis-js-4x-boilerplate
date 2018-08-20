3cd25be85c431d63787a789997900aa6143013cb
ryan0000 <rlibed@esri.com>
2018-08-20 10:05:13 -0700

    Added Locator and Feature layer modules to test app.

--- app/src/js/Main.js
+++ app/src/js/Main.js
@@ -1,2 +1,23 @@
-require(["dojo/domReady!"], function () { });
+require([
+    "esri/WebMap",
+    "esri/views/MapView",
+    "esri/tasks/Locator",
+    "esri/layers/FeatureLayer",
+    "esri/widgets/Search",
+    "dojo/domReady!"
+], function (WebMap, MapView, Locator, FeatureLayer, Search) {
+    var map = new WebMap({
+        portalItem: {
+            id: "18f11672eede4bfea2d5d9dd259bb41d"
+        }
+    });
+    var view = new MapView({
+        map: map,
+        container: "viewDiv"
+    });
+    var search = new Search({
+        view: view
+    });
+    view.ui.add(search, "top-right");
+});
 //# sourceMappingURL=Main.js.map
--- app/src/js/Main.ts
+++ app/src/js/Main.ts
@@ -1,1 +1,22 @@
-require(["dojo/domReady!"], function() {});
+require([
+  "esri/WebMap",
+  "esri/views/MapView",
+  "esri/tasks/Locator",
+  "esri/layers/FeatureLayer",
+  "esri/widgets/Search",
+  "dojo/domReady!"
+], function(WebMap, MapView, Locator, FeatureLayer, Search) {
+  const map = new WebMap({
+    portalItem: {
+      id: "18f11672eede4bfea2d5d9dd259bb41d"
+    }
+  });
+  const view = new MapView({
+    map,
+    container: "viewDiv"
+  });
+  const search = new Search({
+    view
+  });
+  view.ui.add(search, "top-right");
+});
